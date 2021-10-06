15c3f1b32562cbbe67f296f1e87cf84f15922c97
Will Holcombe <will.v.holcombe@gmail.com>
2021-10-06 16:16:12 -0400

    hooking up firebase to signin page

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import React from 'react';
 import {
   Switch,
   Route,
-  BrowserRouter as Router,
-  Redirect
+  Redirect,
+  BrowserRouter as Router
 } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/HomePage';
 import LandingPage from './pages/components/LandingPage';
+import Header from './shared/components/Header/Header';
 
 
 
 function App() {
   return (
-    <Router>
+    <div>
+      <Router>
+      <Header />
       <Switch>
-        <Route to="/">
+        <Route exact path="/">
           <HomePage />
         </Route>
-        <Route to="/auth">
+        <Route path="/auth">
           <LandingPage />
         </Route>
         <Redirect to="auth" />
       </Switch>
-    </Router>
+      </Router>
+    </div>
    
   );
 }
